0a2bff3851e5ec15cacbad4b3120d5ab406373fd
Ren Tatsumoto <tatsu@autistici.org>
2021-06-03 09:32:59 +0300

    refactor gui

--- gui.py
+++ gui.py
@@ -3,8 +3,8 @@ from typing import Dict
 from aqt import mw
 from aqt.qt import *
 
-from .config import config
 from .color import Color
+from .config import config
 
 WINDOW_TITLE = "Mortician Options"
 
@@ -13,28 +13,27 @@ def get_toggleables() -> Dict[str, bool]:
     return {key: val for key, val in config.items() if type(val) == bool}
 
 
+def key_to_label(key: str) -> str:
+    return key.replace('_', ' ').capitalize()
+
+
 def make_checkboxes() -> Dict[str, QCheckBox]:
     checkboxes = {}
     for key, val in get_toggleables().items():
-        label = key.replace('_', ' ').capitalize()
+        label = key_to_label(key)
         checkboxes[key] = QCheckBox(label)
         checkboxes[key].setChecked(config[key])
     return checkboxes
 
 
-def make_marking_widgets() -> Dict[str, QWidget]:
-    tag_widget = QLineEdit(config['tag'])
-    flag_widget = QComboBox()
-    flag_widget.addItems(Color.colors())
-    flag_widget.setCurrentIndex(Color.num_of(config['flag']))
-    flag_widget.text = flag_widget.currentText
-    return {
-        'tag': tag_widget,
-        'flag': flag_widget,
-    }
+def make_flag_edit_widget() -> QComboBox:
+    flag_edit = QComboBox()
+    flag_edit.addItems(Color.colors())
+    flag_edit.setCurrentIndex(Color.num_of(config['flag']))
+    return flag_edit
 
 
-def make_numbers_widgets() -> Dict[str, QWidget]:
+def make_limits_widgets() -> Dict[str, QLineEdit]:
     widgets = {}
     for key in ('again_threshold', 'timeframe'):
         widgets[key] = QLineEdit()
@@ -44,12 +43,13 @@ def make_numbers_widgets() -> Dict[str, QWidget]:
 
 
 class SettingsDialog(QDialog):
-    def __init__(self, flags, *args, **kwargs):
-        super().__init__(flags, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setWindowTitle(WINDOW_TITLE)
         self._checkboxes = make_checkboxes()
-        self._marking = make_marking_widgets()
-        self._numbers = make_numbers_widgets()
+        self._tag_edit = QLineEdit(config['tag'])
+        self._flag_edit = make_flag_edit_widget()
+        self._limits = make_limits_widgets()
         self.ok_button = QPushButton("Ok")
         self.cancel_button = QPushButton("Cancel")
         self.setLayout(self.setup_layout())
@@ -59,31 +59,40 @@ class SettingsDialog(QDialog):
         layout = QVBoxLayout(self)
         layout.addLayout(self.make_checkboxes_layout())
         layout.addLayout(self.make_marking_layout())
-        layout.addLayout(self.make_numbers_layout())
+        layout.addLayout(self.make_limits_layout())
         layout.addLayout(self.make_bottom_buttons())
         return layout
 
     def make_checkboxes_layout(self) -> QGroupBox:
         layout = QVBoxLayout()
-        for _, widget in self._checkboxes.items():
+        for widget in self._checkboxes.values():
             layout.addWidget(widget)
         return layout
 
     def make_marking_layout(self) -> QGridLayout:
         layout = QGridLayout()
-        grid_lines = ((QLabel(key.capitalize()), widget) for key, widget in self._marking.items())
-        for y_idx, line in enumerate(grid_lines):
-            for x_idx, widget in enumerate(line):
-                layout.addWidget(widget, y_idx, x_idx)
+        lines = (
+            (QLabel('Tag'), self._tag_edit),
+            (QLabel('Flag'), self._flag_edit)
+        )
+
+        for y, line in enumerate(lines):
+            for x, widget in enumerate(line):
+                layout.addWidget(widget, y, x)
+
         return layout
 
-    def make_numbers_layout(self) -> QGridLayout:
+    def make_limits_layout(self) -> QGridLayout:
         layout = QGridLayout()
+        lines = zip(
+            (QLabel(key_to_label(key)) for key in self._limits.keys()),
+            self._limits.values(),
+            (QLabel(unit) for unit in ('times', 'hours')),
+        )
 
-        for y_idx, ((key, widget), unit) in enumerate(zip(self._numbers.items(), ('times', 'hours'))):
-            layout.addWidget(QLabel(key.capitalize()), y_idx, 0)
-            layout.addWidget(widget, y_idx, 1)
-            layout.addWidget(QLabel(unit), y_idx, 2)
+        for y, line in enumerate(lines):
+            for x, widget in enumerate(line):
+                layout.addWidget(widget, y, x)
 
         return layout
 
@@ -101,10 +110,10 @@ class SettingsDialog(QDialog):
     def on_confirm(self):
         for key, widget in self._checkboxes.items():
             config[key] = widget.isChecked()
-        for key, widget in self._marking.items():
-            config[key] = widget.text()
-        for key, widget in self._numbers.items():
+        for key, widget in self._limits.items():
             config[key] = int(widget.text())
+        config['tag'] = self._tag_edit.text()
+        config['flag'] = self._flag_edit.currentText()
         mw.addonManager.writeConfig(__name__, config)
         self.accept()
 
